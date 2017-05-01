02cd0b0dcde4c3b49fa4586793e404143c865b31
Sachin Chauhan <Sachin@Sachins-MacBook-Pro.local>
2017-05-01 13:22:26 +0530

    added fragment with recyler view

---
+++ app/src/main/java/com/allinmyapp/news/NewsPagerAdapter.java
@@ -0,0 +1,50 @@
+package com.allinmyapp.news;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import com.allinmyapp.news.UI.NewsFragment;
+
+/**
+ * Created by Sachin on 01/05/17.
+ */
+
+
+/**
+ * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
+ * one of the sections/tabs/pages.
+ */
+public class NewsPagerAdapter extends FragmentPagerAdapter {
+
+    public NewsPagerAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        // getItem is called to instantiate the fragment for the given page.
+        // Return a PlaceholderFragment (defined as a static inner class below).
+        return NewsFragment.newInstance(position + 1);
+    }
+
+    @Override
+    public int getCount() {
+        // Show 3 total pages.
+        return 3;
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        switch (position) {
+            case 0:
+                return "SECTION 1";
+            case 1:
+                return "SECTION 2";
+            case 2:
+                return "SECTION 3";
+        }
+        return null;
+    }
+}
+
